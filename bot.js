32f6c990314ca3e6248b8758601fd143db76ea10
Nathan <nathan13888@gmail.com>
2019-09-14 14:53:39 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,39 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./secret.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `//`
+    if (message.substring(0, 2) == '//') {
+        var args = message.substring(2).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+         }
+     }
+});
